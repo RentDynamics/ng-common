50e8c6c2468aa51a5ed941e38b5d86ea3fbad669
Chase Gibbons <chase@rentdynamics.com>
2018-10-09 16:42:30 -0600

    added back RdAngularCommonModule

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,12 @@
 export * from './expanding-list-view/index';
 export * from './tabset/index';
 export * from './loading-indicator/index';
-export *from './modal/index';
+export * from './modal/index';
 // export {TelephonePipe} from './pipes/index';
 export * from './toast/index';
 export * from './simple-sort/index';
 export * from './upload/index';
-// export {RdAngularCommonModule} from './common.module';
+export {RdAngularCommonModule} from './common.module';
 export * from './iframe/index';
 export * from './conversation/index';
 export * from './pipes/index';
