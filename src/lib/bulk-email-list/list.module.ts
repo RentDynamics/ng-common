6792f5154dbfe5ac33e719416958ff9ba938542a
jbsk8nc <joshua.aaron.brown@gmail.com>
2018-12-26 11:51:09 -0700

    removing unused import

--- src/lib/bulk-email-list/list.module.ts
+++ src/lib/bulk-email-list/list.module.ts
@@ -1,6 +1,6 @@
 import { Subscription, Observable } from 'rxjs';
 
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ModalModule } from '../modal/modal.module';
